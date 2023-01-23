3aa93909fe400bd43475196c3f5a127f45ff965d
ashpool <ashpoolin@protonmail.com>
2023-01-23 11:43:28 -0700

    add provisions for allowed URLs

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3001
 
 const query_server = require('./query_server')
 
+const ALLOWED_URL_1 = process.env.ALLOWED_URL_1;
+const ALLOWED_URL_2 = process.env.ALLOWED_URL_2;
+const ALLOWED_URL_3 = process.env.ALLOWED_URL_3;
+const ALLOWED_URL_4 = process.env.ALLOWED_URL_4;
+const ALLOWED_URL_5 = process.env.ALLOWED_URL_5;
+const ALLOWED_URL_6 = process.env.ALLOWED_URL_6;
+
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); //this may only allow localhost!
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  const allowedOrigins = ['http://localhost:3000', ALLOWED_URL_1, ALLOWED_URL_2, ALLOWED_URL_3, ALLOWED_URL_4, ALLOWED_URL_5, ALLOWED_URL_6];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); //this may only allow localhost!
+  // res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
 });
