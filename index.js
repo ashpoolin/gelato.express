4cc02ad55c707581c9065f0e730a48e9103a80ca
ashpool <ashpoolin@protonmail.com>
2023-01-25 11:16:56 -0700

    add exchange balance query

--- index.js
+++ index.js
@@ -7,28 +7,17 @@ var cors = require('cors');
 
 const query_server = require('./query_server')
 
-// const ALLOWED_URL_1 = process.env.ALLOWED_URL_1;
-// const ALLOWED_URL_2 = process.env.ALLOWED_URL_2;
-// const ALLOWED_URL_3 = process.env.ALLOWED_URL_3;
-// const ALLOWED_URL_4 = process.env.ALLOWED_URL_4;
-// const ALLOWED_URL_5 = process.env.ALLOWED_URL_5;
-// const ALLOWED_URL_6 = process.env.ALLOWED_URL_6;
-
 app.use(cors())
-// app.use(express.json())
-// app.use(function (req, res, next) {
-//   const allowedOrigins = ['http://localhost:3000', ALLOWED_URL_1, ALLOWED_URL_2, ALLOWED_URL_3, ALLOWED_URL_4, ALLOWED_URL_5, ALLOWED_URL_6];
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//        res.setHeader('Access-Control-Allow-Origin', origin);
-//   }
-//   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); //this may only allow localhost!
-//   // res.header("Access-Control-Allow-Origin", "*");
-//   res.setHeader('Access-Control-Allow-Methods', 'GET');
-//   // res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-//   next();
-// });
+
+app.get('/balances', (req, res) => {
+  query_server.getLatestBalances()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
 
 app.get('/events', (req, res) => {
   query_server.getLatestEvents()
@@ -49,6 +38,8 @@ app.get('/:exchange', (req, res) => {
     res.status(500).send(error);
   })
 })
+
+
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
--- query_server.js
+++ query_server.js
@@ -23,6 +23,17 @@ const after = (new Date(Date.now() - 604800000)).toISOString().split('T')[0];
     }) 
   }
 
+  const getLatestBalances = () => {
+    return new Promise(function(resolve, reject) {
+      pool.query(`SELECT *, FLOOR(latest_balance / (select sum(latest_balance) from latest_exchange_balances) * 100 * 100) / 100 as pct_share FROM latest_exchange_balances order by latest_balance DESC;`, (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(results.rows);
+      })
+    }) 
+  }
+
   const getLatestEvents = () => {
     return new Promise(function(resolve, reject) {
       pool.query(`select dt, slot, owner, floor(sol) as sol, floor(delta) as delta from sol_delta_data order by dt desc limit 50`, (error, results) => {
@@ -33,8 +44,11 @@ const after = (new Date(Date.now() - 604800000)).toISOString().split('T')[0];
       })
     }) 
   }
+
+
   
   module.exports = {
     getExchangeBalance,
-    getLatestEvents
+    getLatestEvents,
+    getLatestBalances
   }
